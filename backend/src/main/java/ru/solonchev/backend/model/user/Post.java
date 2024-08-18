bc8f478347945636725de9edb1ca3cb56c6a2cdf
Mark Solonchev <solonchev32@gmail.com>
2024-08-18 20:16:16 +0300

    add post entity && db info

---
+++ backend/src/main/java/ru/solonchev/backend/model/user/Post.java
@@ -0,0 +1,21 @@
+package ru.solonchev.backend.model.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "post_name", unique = true, nullable = false, length = 512)
+    private String postName;
+}
